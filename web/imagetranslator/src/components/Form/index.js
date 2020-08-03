3698e5c4027851f8bdbaca09785f596b8f57d4c4
Matheus Alves Lemos <matheus@osm.com.br>
2020-08-03 00:51:35 -0300

    Criação do botão de upload de carregar o base64 da imagem

--- web/imagetranslator/src/components/Form/index.js
+++ web/imagetranslator/src/components/Form/index.js
@@ -1,12 +1,14 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect, useRef} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import TextField from '@material-ui/core/TextField';
 
 
 function Form(){
-const [fileState,setFileState]=useState();   
+const [fileState,setFileState]=useState(); 
+const fileInput = useRef(null);
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: "50px 600px",
@@ -22,8 +24,20 @@ const useStyles = makeStyles((theme) => ({
 
   }));
  
-  
+  function fileNames() {
+    const { current } = fileInput;
+    if (current && current.files.length > 0) {
+      let messages = [];
+      for (let file of current.files) {
+        messages = messages.concat(<p key={file.name}>{file.name}</p>);
+      }
+      return messages;
+    }
+    return null;
+  }
+
   const handleFileUploaded = e => {
+    setFileState();
     const reader = new FileReader();
     const file = e.target.files[0];
     reader.onloadend = () => {
@@ -35,37 +49,36 @@ const useStyles = makeStyles((theme) => ({
     const classes = useStyles();
     return(
         <div className={classes.root}>
-        <div>
-            Upload de imagens para testar o serviço IBM/WATSON
-        </div>
-            <input
-                accept="image/*"
-                className={classes.input}
-                id="contained-button-file"
-                multiple
-                type="file"
-                onChange={handleFileUploaded}
-                encType="multipart/form-data"   
-            />
-            <label htmlFor="contained-button-file">
-                <Button variant="contained" color="primary" component="span">
-                Upload
-                </Button>
-            </label>
-            <input 
-                accept="image/*" 
-                className={classes.input} 
-                id="icon-button-file" 
-                type="file" 
-                onChange={handleFileUploaded}
-                encType="multipart/form-data"                
-                />
-            <label htmlFor="icon-button-file">
-                <IconButton color="primary" aria-label="upload picture" component="span">
-                <PhotoCamera />
-                </IconButton>
-            </label>
+            <div>
+                Upload de imagens para testar o serviço IBM/WATSON
             </div>
+                <input 
+                    ref={fileInput}
+                    accept="image/*" 
+                    className={classes.input} 
+                    id="icon-button-file" 
+                    type="file" 
+                    onChange={handleFileUploaded}
+                    encType="multipart/form-data"                
+                    />
+                <label htmlFor="icon-button-file">
+                    <IconButton color="primary" aria-label="upload picture" component="span">
+                    <PhotoCamera />
+                    </IconButton>
+                </label>
+                <div>
+                    <label>
+                        Valor Imagem Base 64
+                    </label>
+                    {fileState}
+                </div>
+                <div>
+                    <label>
+                        Nome da Imagem
+                    </label>
+                    {fileNames()}
+                </div>
+        </div>
     );
 }
 
